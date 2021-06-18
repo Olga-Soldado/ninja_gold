146dc48f72c0156ffd73694bda884425ab991dfb
Olga-Soldado <valentina.soldado.1994@gmail.com>
2021-06-17 22:03:18 -0400

    Agregando diseño

--- ninja_gold/views.py
+++ ninja_gold/views.py
@@ -8,19 +8,19 @@ def index(request):
 
 def process_money(request):
     if 'count' in request.session:
-        if request.POST['building'] == 'farm':
+        if request.POST['building'] == 'Granja':
             score = random.randint(10,20)
-        elif request.POST['building'] == 'cave':
+        elif request.POST['building'] == 'Cueva':
             score = random.randint(5,10)
-        elif request.POST['building'] == 'house':
+        elif request.POST['building'] == 'Casa':
             score = random.randint(2,5)
-        elif request.POST['building'] == 'casino':
+        elif request.POST['building'] == 'Casino':
             score = random.randint(-50,50)
         request.session['count'] += score
         if score > 0:
-            request.session['box'].insert(0,{'class': 'green', 'log': f"Haz ganado  {score} puntos oro de la {request.POST['building']}! {strftime('%Y/%m/%d %I:%M:%S %p')}"})
+            request.session['box'].insert(0,{'class': 'green', 'log': f"Haz ganado  {score} puntos XD {request.POST['building']}! {strftime('%Y/%m/%d %I:%M:%S %p')}"})
         else:
-            request.session['box'].insert(0,{'class': 'red', 'log': f"Haz perdido {-1*score} golds at the {request.POST['building']}... Ouch!!! {strftime('%Y/%m/%d %I:%M:%S %p')}"})
+            request.session['box'].insert(0,{'class': 'red', 'log': f"Haz perdido {-1*score} puntos{request.POST['building']}... Ouch!!! =({strftime('%Y/%m/%d %I:%M:%S %p')}"})
     else:
         request.session['count'] = 0
         request.session['box'] = []
